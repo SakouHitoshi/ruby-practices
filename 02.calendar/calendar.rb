db10c9270c143d299554b833873a6c7ed9e29a57
Hitoshi Sakou <grahamandhitoshi@gmail.com>
2020-12-04 20:09:06 +0900

    カレンダー課題修正_01

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -1,4 +1,3 @@
-#!/usr/bin/env ruby
 require "optparse" #optparseクラスのデータを受け取る
 require 'date' #dateクラスのデータを受け取る
 day = Date.today #今日の情報
@@ -16,18 +15,19 @@ else
   month = day.mon #入力されないと今月のデータになる
 end
 
-firstday_week = Date.new(year,month,1).wday #今月1日の曜日を取得(0~6)
-lastday = Date.new(year,month,-1).day #今月の最終日を取得
+firstday = Date.new(year, month,1) #今月の1日を取得
+firstday_week = Date.new(year, month,1).wday #今月1日の曜日を取得(0~6)
+lastday = Date.new(year, month,-1) #今月の最終日を取得
 week = "日 月 火 水 木 金 土" #曜日を表示
 
-puts  "#{month}月 #{year}".center(20) #month変数とyear変数を出力
+puts "#{month}月 #{year}".center(20) #month変数とyear変数を出力
 puts week #曜日を出力
 print "   " * firstday_week #1日までの空白を出力
 
-(1..lastday).each do |day| #1日から最終日まで繰り返し
-  print day.to_s.rjust(2) + " " #文字列に変換した後に日付を右寄せで表示
-  firstday_week = firstday_week + 1 #1日目の曜日数から1足していく
-  if firstday_week % 7 == 0 #7で割り切れる数は土曜日なので土曜日まで表示したら改行
-    print "\n"
-  end
+(firstday..lastday).each do |d| #1日から最終日まで繰り返し
+  print d.day.to_s.rjust(2) + " " #文字列に変換した後に日付を右寄せで表示
+  if d.saturday? #土曜日まで表示したら改行
+      print "\n"
+    end
 end
+print "\n"
