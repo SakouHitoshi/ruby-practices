cd94e40394b150968612b831cbd1b52443afee22
Hitoshi Sakou <grahamandhitoshi@gmail.com>
2020-12-17 16:40:07 +0900

    ボウリングプログラム_改正01

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -50,7 +50,7 @@ frames.each_with_index do |f, i|
                20 + frames[i + 2][0]
              elsif frames[i][0] == 10
                10 + frames[i + 1][0] + frames[i + 1][1]
-             elsif frame.sum == 10 && frames[0] != 10
+             elsif f.sum == 10 && frames[0] != 10
                10 + frames[i + 1][0]
              else
                f.sum
