81769b2ee5ce46d30d4b9ed855bef50b7704da24
Hitoshi Sakou <grahamandhitoshi@gmail.com>
2020-12-31 15:25:55 +0900

    Update bowling.rb ver2

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -12,61 +12,28 @@ scores.each do |s|
            else
              s.to_i
            end
-
   if frame.count == 1 && frame.first == 10
     frames << frame
     frame = []
   elsif frame.count == 2
     frames << frame
     frame = []
-
+  elsif frames.size >= 9
+    frames << frame
+    frame = []
   end
-end
-if frames[9] == [10] && frames[10] == [10] && frames[11] == [10]
-  frames[9].concat frames[10] + frames[11]
-  frames.delete_at(10)
-  frames.delete_at(10)
-elsif frames[9] == [10] && frames[10] == [10]
-  frames[9].concat frames[10]
-  frames[9] << scores[-1].to_i
-  frames.delete_at(10)
-elsif frames[9] == [10]
-  frames[9].concat frames[10]
-  frames.delete_at(10)
-elsif frames[9] != [10] && frames[9].sum == 10 && frames[10] == [10]
-  frames[9].concat frames[10]
-  frames.delete_at(10)
-elsif frames[9] != [10] && frames[9].sum == 10
-  frames[9] << scores[-1].to_i
-  frames.delete_at(10)
 end
 
 point = 0
 frames.each_with_index do |f, i|
-  point += if i != 8 && i != 9
-             if frames[i][0] == 10 && frames[i + 1][0] == 10 && frames[i + 2][0] == 10
-               30
-             elsif frames[i][0] == 10 && frames[i + 1][0] == 10
-               20 + frames[i + 2][0]
-             elsif frames[i][0] == 10
-               10 + frames[i + 1][0] + frames[i + 1][1]
-             elsif f.sum == 10 && frames[0] != 10
-               10 + frames[i + 1][0]
-             else
-               f.sum
-             end
-           elsif i == 8
-             if frames[i][0] == 10 && frames[i + 1][0] == 10 && frames[i + 1][1] == 10
-               30
-             elsif frames[i][0] == 10 && frames[i + 1][0] == 10
-               20 + frames[i + 1][1]
-             elsif frames[i][0] == 10
-               10 + frames[i + 1][0] + frames[i + 1][1]
-             elsif f.sum == 10 && frames[0] != 10
-               10 + frames[i + 1][0]
-             else
-               f.sum
-             end
+  point += if i < 9 && frames[i][0] == 10 && frames[i + 1][0] == 10
+             20 + frames[i + 2][0]
+           elsif i == 8 && frames[i][0] == 10
+             10 + frames[i + 1][0] + frames[i + 2][0]
+           elsif i < 9 && frames[i][0] == 10
+             10 + frames[i + 1][0] + frames[i + 1][1]
+           elsif i < 9 && f.sum == 10 && frames[0] != 10
+             10 + frames[i + 1][0]
            else
              f.sum
            end
