45c59ae3a0cf7409f358e098d90eac09930f08f4
Hitoshi Sakou <grahamandhitoshi@gmail.com>
2021-01-02 17:22:52 +0900

    Update bowling.rb ver3

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -5,35 +5,48 @@ scores = score.chars
 
 frames = []
 frame = []
+STRIKE = 10
+NINE_FRAME = 9
 
 scores.each do |s|
   frame << if s == 'X'
-             10
+             STRIKE
            else
              s.to_i
            end
-  if frame.count == 1 && frame.first == 10
+  if frame.count == 1 && frame.first == STRIKE
     frames << frame
     frame = []
   elsif frame.count == 2
     frames << frame
     frame = []
-  elsif frames.size >= 9
+  elsif frames.size >= NINE_FRAME
     frames << frame
     frame = []
   end
 end
 
+if frames[11]
+  frames[9].concat frames[10] + frames[11]
+  frames.delete_at(10)
+  frames.delete_at(10)
+elsif frames[10]
+  frames[9].concat frames[10]
+  frames.delete_at(10)
+end
+
 point = 0
 frames.each_with_index do |f, i|
-  point += if i < 9 && frames[i][0] == 10 && frames[i + 1][0] == 10
-             20 + frames[i + 2][0]
-           elsif i == 8 && frames[i][0] == 10
-             10 + frames[i + 1][0] + frames[i + 2][0]
-           elsif i < 9 && frames[i][0] == 10
-             10 + frames[i + 1][0] + frames[i + 1][1]
-           elsif i < 9 && f.sum == 10 && frames[0] != 10
-             10 + frames[i + 1][0]
+  point += if i == 8 && frames[i][0] == STRIKE && frames[i + 1][0] == STRIKE
+             STRIKE * 2 + frames[i + 1][1]
+           elsif i < NINE_FRAME && frames[i][0] == STRIKE && frames[i + 1][0] == STRIKE
+             STRIKE * 2 + frames[i + 2][0]
+           elsif i == 8 && frames[i][0] == STRIKE
+             STRIKE + frames[i + 1][0] + frames[i + 1][1]
+           elsif i < NINE_FRAME && frames[i][0] == STRIKE
+             STRIKE + frames[i + 1][0] + frames[i + 1][1]
+           elsif i < NINE_FRAME && f.sum == STRIKE && frames[0] != STRIKE
+             STRIKE + frames[i + 1][0]
            else
              f.sum
            end
