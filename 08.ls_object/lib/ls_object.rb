9928fbdba6862ef8db217a5b203b946448ae2a91
Hitoshi Sakou <grahamandhitoshi@gmail.com>
2021-05-21 22:16:24 +0900

    Option & ShortFormat & Commandクラス定義

--- 08.ls_object/lib/ls_object.rb
+++ 08.ls_object/lib/ls_object.rb
@@ -2,55 +2,58 @@ require 'optparse'
 require 'etc'
 
 class Command
-  def initialize
+  attr_reader :files
 
+  def initialize
+    @files = Dir.glob('*').sort
+  end
+
+  def output
+    if @option.l_option
+      LongFormat.new(@files)
+    else
+      ShortFormat.new(@files).display_arrange
+    end
   end
 end
 
-class FileDate
-  def initialize
-
-  end
-end
-
-class Option
-  def initialize(option = ARGV.getopts('arl'))
+class Opition
+  def initialize(option)
     @option = option
   end
 
-  def a_option
-    @option['a']
-  end
-
-  def r_option
-    @option['r']
-  end
-
-  def l_option
-    @option['l']
+  def option_files
+    file_names = @option['a'] ? Dir.glob('*', File::FNM_DOTMATCH) : Dir.glob('*')
+    @option['r'] ? file_names.sort.reverse : file_names.sort
   end
 end
 
-class NormalFormatter
+class ShortFormat
+  attr_reader :files
+
+  def initialize(files)
+    @files = files
+  end
+
+  def display_arrange
+    array = files.each_slice(4).map { |sub_file_names| Array.new(4) { sub_file_names.shift } }
+    array.transpose.each do |record|
+      record.each do |display|
+        print display.to_s.ljust(20)
+      end
+      print "\n"
+    end
+  end
+end
+
+class LongFormat
+  attr_reader :files
+
   def initialize
-
+    @files = files
   end
 end
 
-class AllFile
-  def initialize
-
-  end
-end
-
-class LongFormatter
-  def initialize
-
-  end
-end
-
-class Reversing
-  def initialize
-
-  end
-end
+option = ARGV.getopts('a', 'l', 'r')
+files = Opition.new(option).option_files
+ShortFormat.new(files).display_arrange
