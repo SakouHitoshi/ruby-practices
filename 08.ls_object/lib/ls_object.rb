3bec3aa14872dad94e127913cbb96d0e4c54b743
Hitoshi Sakou <grahamandhitoshi@gmail.com>
2021-05-22 13:36:54 +0900

    ShortFormat & ShortFormat & Commandクラス完成

--- 08.ls_object/lib/ls_object.rb
+++ 08.ls_object/lib/ls_object.rb
@@ -2,29 +2,16 @@ require 'optparse'
 require 'etc'
 
 class Command
-  attr_reader :files
+  attr_reader :files, :option
 
-  def initialize
-    @files = Dir.glob('*').sort
+  def initialize(option)
+    @option = option
+    @files = option['a'] ? Dir.glob('*', File::FNM_DOTMATCH) : Dir.glob('*')
+    files.reverse! if option['r']
   end
 
   def output
-    if @option.l_option
-      LongFormat.new(@files)
-    else
-      ShortFormat.new(@files).display_arrange
-    end
-  end
-end
-
-class Opition
-  def initialize(option)
-    @option = option
-  end
-
-  def option_files
-    file_names = @option['a'] ? Dir.glob('*', File::FNM_DOTMATCH) : Dir.glob('*')
-    @option['r'] ? file_names.sort.reverse : file_names.sort
+    option['l'] ? LongFormat.new(files).l_option_list : ShortFormat.new(files).display_arrange
   end
 end
 
@@ -49,11 +36,63 @@ end
 class LongFormat
   attr_reader :files
 
-  def initialize
+  def initialize(files)
     @files = files
+  end
+
+  def l_option_list
+    total_blocks
+    files.each do |file|
+      print file_type(file)
+      print file_permission(file)
+      print File::Stat.new(file).nlink.to_s.ljust(2)
+      print Etc.getpwuid(File.stat(file).uid).name.ljust(14)
+      print Etc.getgrgid(File.stat(file).gid).name.ljust(6)
+      print File::Stat.new(file).size.to_s.rjust(6)
+      print File::Stat.new(file).mtime.strftime(' %m %d %H:%M ').rjust(12)
+      puts file
+    end
+  end
+
+  def total_blocks
+    total_blocks = files.sum do |file|
+      File::Stat.new(file).blocks
+    end
+    puts "total #{total_blocks}"
+  end
+
+  def file_type(file)
+    lists = {
+      'file' => '-',
+      'directory' => 'd',
+      'characterSpecial' => 'c',
+      'blockSpecial' => 'b',
+      'fifo' => 'f',
+      'link' => 'l',
+      'socket' => 's',
+      'unknown' => '?'
+    }
+    lists[File::Stat.new(file).ftype]
+  end
+
+  def file_permission(file)
+    permission = File::Stat.new(file).mode.to_s(8).slice(-3, 3).chars
+    permission.map! do |n|
+      lists = {
+        '7' => 'rwx',
+        '6' => 'rw-',
+        '5' => 'r-x',
+        '4' => 'r--',
+        '3' => '-wx',
+        '2' => '-w-',
+        '1' => '--x',
+        '0' => '---'
+      }
+      lists[n]
+    end
+    permission.join
   end
 end
 
 option = ARGV.getopts('a', 'l', 'r')
-files = Opition.new(option).option_files
-ShortFormat.new(files).display_arrange
+Command.new(option).output
